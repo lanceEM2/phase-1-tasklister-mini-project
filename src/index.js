aea8255133b7d9450ff0e8eac83b90917c872a60
Dylan <dylanlancekemboi@gmail.com>
2023-10-16 12:43:38 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskDescription = taskInput.value;
+    if (taskDescription) {
+      const listItem = document.createElement("li");
+      listItem.textContent = taskDescription;
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      listItem.appendChild(deleteButton);
+      taskList.appendChild(listItem);
+
+      deleteButton.addEventListener("click", function () {
+        taskList.removeChild(listItem);
+      });
+
+      taskInput.value = "";
+    }
+  });
 });
